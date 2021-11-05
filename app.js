2af7ce76aaeafbc99bbe47ffb1b6efba43faacd6
Luke Ganter <lucaseganter18@gmail.com>
2021-11-04 20:53:45 -0600

    fixed the grades

--- app.js
+++ app.js
@@ -63,17 +63,18 @@ element.innerHTML='Check out The Specital Today' + '</br>' + specail();
 function assignGrade(score){
 if(score > 90) {
     return 'A';
-}else if(score > 80 || score < 90) {
+}else if(score > 80 && score < 90) {
     return 'B';
-}else if(score > 70 || score < 80){
+}else if(score > 70 && score < 80){
     return 'C';
-}else if(score > 60 || score < 70){
+}else if(score > 60 && score < 70){
     return 'D';
 }else {
     return 'F';
 }
 }
 
-console.log(`You got a` + assignGrade(95))
-console.log(`You got a` + assignGrade(68))
-console.log(`You got a` + assignGrade(22))
+console.log(`You got  ` + assignGrade(95));
+console.log(`You got  ` + assignGrade(68));
+console.log(`You got  ` + assignGrade(22));
+console.log(`You got  ` + assignGrade(78));
